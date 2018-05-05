30d4612ec5c087a88bc39eb2997a528eb3943e42
xshame <kevin.camu07@gmail.com>
2018-05-05 10:01:04 +0800

    added files to master branch

---
+++ assets/js/page-scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  //Page scroll
+  var headerHeight = $("header").outerHeight(); //Get this the header Height
+  $(".slide-section").on("click", function(e){
+    var linkHref = $(this).attr("href"); //this is to get the href value.
+    $("html, body").animate({
+      scrollTop: $(linkHref).offset().top
+    }, 1000);
+    e.preventDefault();//just to prevent the default value on the page
+  });
+});
